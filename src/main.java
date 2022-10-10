28f4380546f5df8cd4ec2f15c90eb06594411200
Alvaro-R <67501790+Alvaro-R@users.noreply.github.com>
2022-10-10 19:43:56 +0200

    mbmbn

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(" mundo");
+		System.out.println("hkh mundo");
 		
 
 	}
