93da15291be0510dca832cfa54ff4ca4ea8aae03
Alvaro-R <67501790+Alvaro-R@users.noreply.github.com>
2022-10-10 19:18:03 +0200

    dfhgfhgfh

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hodfgfhgf mundo");
+		System.out.println("Hfhgf mundo");
 		
 
 	}
